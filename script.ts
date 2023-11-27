4b8bd9b7154286e047b53f62a0a0b6923b8827e9
root <root@ip-10-0-1-73.us-west-1.compute.internal>
2023-11-27 14:18:53 +0000

    Second[Agent] javascript to typescript

--- script.ts
+++ script.ts
@@ -1,15 +1,15 @@
-const word = document.getElementById('word');
-const text = document.getElementById('text');
-const scoreEl = document.getElementById('score');
-const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game-container');
-const settingsBtn = document.getElementById('settings-btn');
-const settings = document.getElementById('settings');
-const settingsForm = document.getElementById('settings-form');
-const difficultySelect = document.getElementById('difficulty');
+const word = document.getElementById('word') as HTMLElement;
+const text = document.getElementById('text') as HTMLInputElement;
+const scoreEl = document.getElementById('score') as HTMLElement;
+const timeEl = document.getElementById('time') as HTMLElement;
+const endgameEl = document.getElementById('end-game-container') as HTMLElement;
+const settingsBtn = document.getElementById('settings-btn') as HTMLElement;
+const settings = document.getElementById('settings') as HTMLElement;
+const settingsForm = document.getElementById('settings-form') as HTMLFormElement;
+const difficultySelect = document.getElementById('difficulty') as HTMLSelectElement;
 
 // List of words for game
-const words = [
+const words: string[] = [
   'sigh',
   'tense',
   'airplane',
@@ -33,16 +33,16 @@ const words = [
 ];
 
 // Init word
-let randomWord;
+let randomWord: string;
 
 // Init score
-let score = 0;
+let score: number = 0;
 
 // Init time
-let time = 10;
+let time: number = 10;
 
 // Set difficulty to value in ls or medium
-let difficulty =
+let difficulty: string =
   localStorage.getItem('difficulty') !== null
     ? localStorage.getItem('difficulty')
     : 'medium';
@@ -60,24 +60,24 @@ text.focus();
 const timeInterval = setInterval(updateTime, 1000);
 
 // Generate random word from array
-function getRandomWord() {
+function getRandomWord(): string {
   return words[Math.floor(Math.random() * words.length)];
 }
 
 // Add word to DOM
-function addWordToDOM() {
+function addWordToDOM(): void {
   randomWord = getRandomWord();
   word.innerHTML = randomWord;
 }
 
 // Update score
-function updateScore() {
+function updateScore(): void {
   score++;
-  scoreEl.innerHTML = score;
+  scoreEl.innerHTML = score.toString();
 }
 
 // Update time
-function updateTime() {
+function updateTime(): void {
   time--;
   timeEl.innerHTML = time + 's';
 
@@ -89,7 +89,7 @@ function updateTime() {
 }
 
 // Game over, show end screen
-function gameOver() {
+function gameOver(): void {
   endgameEl.innerHTML = `
     <h1>Time ran out</h1>
     <p>Your final score is ${score}</p>
@@ -104,15 +104,15 @@ addWordToDOM();
 // Event listeners
 
 // Typing
-text.addEventListener('input', e => {
-  const insertedText = e.target.value;
+text.addEventListener('input', (e: Event) => {
+  const insertedText = (e.target as HTMLInputElement).value;
 
   if (insertedText === randomWord) {
     addWordToDOM();
     updateScore();
 
     // Clear
-    e.target.value = '';
+    (e.target as HTMLInputElement).value = '';
 
     if (difficulty === 'hard') {
       time += 2;
@@ -130,7 +130,7 @@ text.addEventListener('input', e => {
 settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
 
 // Settings select
-settingsForm.addEventListener('change', e => {
-  difficulty = e.target.value;
+settingsForm.addEventListener('change', (e: Event) => {
+  difficulty = (e.target as HTMLSelectElement).value;
   localStorage.setItem('difficulty', difficulty);
 });
